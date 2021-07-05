f4d4f373bf87154afe513108fbac9fef2a88601d
Hugo Souza <contato@hugosouza.com>
2021-07-05 18:59:42 -0400

    remove chrono

---
+++ src/generate.cpp
@@ -0,0 +1,10 @@
+#include "generate.h"
+
+using namespace std;
+
+vector<int> generateMoves(Board b){
+    vector<int> moves;
+
+
+    return moves;
+}
---
+++ src/generate.h
@@ -0,0 +1,11 @@
+#ifndef GENERATE
+#define GENERATE
+
+#include "board.h"
+
+#include <vector>
+using namespace std;
+
+vector<int> generateMoves(Board b);
+
+#endif
