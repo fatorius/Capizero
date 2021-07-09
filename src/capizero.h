d361f2e4103548927441758009d6ac18586bd21e
Hugo Souza <contato@hugosouza.com>
2021-07-09 15:44:18 -0400

    check if user move is valid

--- src/capizero.h
+++ src/capizero.h
@@ -23,10 +23,31 @@ void read(){
         string move;
         cin>>move;
 
-        board.makeMove(move);
+        bool valid = false;
+
+        vector<string> m = returnMoves(board);
+        for (unsigned int i = 0; i < m.size(); i++){
+            if (move == m[i]){
+                valid = true;
+                break;
+            }
+        }
+
+        if (valid){
+            board.makeMove(move);
+        }
+        else{
+            cout<<move<<" isnt a valid move"<<endl;
+        }
+
     }
     else if (cmd == "show"){
         board.printBoard();
+    }
+    else if (cmd == "help"){
+        cout<<"moves -> print all valid moves on the position"<<endl;
+        cout<<"move [san] -> make a move on the board"<<endl;
+        cout<<"show -> print the board"<<endl;
     }
     else{
         cout<<cmd<<" not a valid command"<<endl;
