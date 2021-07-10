1ecbee58839a8372a22e2ce056989d2ded17f85d
Hugo Souza <contato@hugosouza.com>
2021-07-10 14:50:42 -0400

    Update perft.cpp

--- src/perft.cpp
+++ src/perft.cpp
@@ -16,13 +16,12 @@ u64 perft(int depth, Board pos, bool first = true){
     for (int i = 0; i < moves.size(); i++){
         string backUpFen = pos.fen();
         pos.makeMove(moves[i]);
-        if (!isCheck(pos)){
-            u64 n = perft(depth-1, pos, false);
-            if (first){
-                cout<<moves[i]<<" -> "<<n<<endl;
-            }
-            nodes += n;
+        u64 n = perft(depth-1, pos, false);
+        if (first){
+            cout<<moves[i]<<" -> "<<n<<endl;
         }
+        nodes += n;
+        
         pos.setPosition(backUpFen);
     }
     if (first){
@@ -33,11 +32,30 @@ u64 perft(int depth, Board pos, bool first = true){
 }
 
 bool calc_perft(){
+    string fen;
+    cout<<"fen (s to use starting position): ";
+    cin>>fen;
+    if (fen == "s"){
+        fen = INITIAL_POSITION;
+    }
+    else{
+        string f2;
+        string f3;
+        string f4;
+        string f5;
+        string f6;
+        cin>>f2;
+        cin>>f3;
+        cin>>f4;
+        cin>>f5;
+        cin>>f6;
+        fen += " "+f2+" "+f3+" "+f4+" "+f5+" "+f6;
+    }
     cout<<"depth: ";
     int depth;
     cin>>depth;
 
-    Board pos;
+    Board pos(fen);
     cout<<perft(depth, pos)<<endl<<endl;
 
     return depth;
